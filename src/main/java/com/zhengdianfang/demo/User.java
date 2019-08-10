795e10911cfc047af3f1ca2f8e6b78497b53ee2e
threey-y <52376594+threey-y@users.noreply.github.com>
2019-08-10 14:23:54 +0800

    Update User.java

--- src/main/java/com/zhengdianfang/demo/User.java
+++ src/main/java/com/zhengdianfang/demo/User.java
@@ -1,38 +1,36 @@
 package com.zhengdianfang.demo;
-
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class User {
-
     private String username;
-
     private int age;
-
     private String address;
-
     public String getUsername() {
         return username;
     }
 
+    @Value("${com.user.name}")
     public User setUsername(String username) {
         this.username = username;
         return this;
     }
-
     public int getAge() {
         return age;
     }
 
+
+    @Value("${com.user.age}")
     public User setAge(int age) {
         this.age = age;
         return this;
     }
-
     public String getAddress() {
         return address;
     }
 
+    @Value("${com.user.address}")
     public User setAddress(String address) {
         this.address = address;
         return this;
